cfb0dc5de2b6a1a8b33af7c8fe4e5c479f4c805d
Simon <157841549+SimonGarcia01@users.noreply.github.com>
2026-06-04 15:31:06 -0500

    enabled cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
+    //Needed to connect to the apollo server
+    app.enableCors();
+
     app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true, forbidNonWhitelisted: true }));
 
     await app.listen(process.env.PORT ?? 3001);
