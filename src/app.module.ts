2c23e1629dff2b5c104fd3648d7ffcec6263b7d9
Simon <157841549+SimonGarcia01@users.noreply.github.com>
2026-06-04 15:47:23 -0500

    Removed double endpoint to the same thing?

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,6 @@ import { SeedModule } from './seed/seed.module';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './common/guards/auth.guard';
 import { PermissionsGuard } from './common/guards/permission.guard';
-import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 
 type SupportedDbTypes = 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mongodb' | 'oracle';
 
@@ -25,7 +24,6 @@ type SupportedDbTypes = 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mongodb' 
             introspection: true,
             csrfPrevention: false,
             playground: true,
-            plugins: [ApolloServerPluginLandingPageLocalDefault()],
         }),
         TypeOrmModule.forRootAsync({
             imports: [ConfigModule],
